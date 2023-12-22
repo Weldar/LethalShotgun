ff3a19c2f17407fa18157d9a04c01022d16ee33b
Weldar <2402337-Weldar@users.noreply.gitlab.com>
2023-12-21 19:06:54 -0500

    Initial commit.

---
+++ LethalShotgun/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LethalShotgun
+{
+    [HarmonyPatch]
+    [BepInPlugin("com.github.weldar.lethalshotgun", "LethalShotgun", "1.0.0")]
+    [BepInProcess("Lethal Company.exe")]
+    public class LethalShotgun : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin com.github.weldar.lethalshotgun is loaded!");
+            var harmony = new Harmony("com.github.weldar.lethalshotgun");
+            harmony.PatchAll();
+        }
+
+
+        [HarmonyPatch(typeof(Item), "ScriptableObject")]
+        [HarmonyPostfix]
+
+        public static void Postfix(Item __instance)
+        {
+            if (__instance.itemName == "Shotgun")
+            {
+                __instance.isScrap = false;
+            }
+        }
+    }
+}
---
+++ LethalShotgun/obj/Debug/net46/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalShotgun
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LethalShotgun";
+        public const string PLUGIN_NAME = "LethalShotgun";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
